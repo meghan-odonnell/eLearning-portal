1f2a63482619fecf24e5f431c249b94de239c420
Ang Young <angieyou3@gmail.com>
2023-04-11 22:47:06 -0400

    changes to login

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -77,192 +77,87 @@ export default {
 </script>
 
 <style scoped>
-body {
-   background-image: url("../img/image.png");
-    
+
+
+#login {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+  font-family: 'Didact Gothic', sans-serif;
+}
+
+h1 {
+  font-size: 36px;
+  font-family: 'Comfortaa', cursive;
+  margin-top: 0;
+  text-align: center;
+}
+
+h2 {
+  font-size: 28px;
+  font-family: 'Comfortaa', cursive;
+  margin-top: 0;
+  text-align: center;
+}
+
+h3 {
+  font-size: 24px;
+  margin-top: 0;
+  margin-bottom: 20px;
+  text-align: center;
 }
 
 .form-input-group {
-  margin-bottom: 1rem;
-}
-
-.math {
-    /* margin-left: 30px; */
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-    text-align: center;
-  }
-
-h1 { 
-    color: rgb(0, 4, 10);
-    font-family: "Comfortaa";
-    font-size: 50px;
-    max-width: 100%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-    text-align: center;
-
-}
-
-h2 { 
-    color: rgb(0, 4, 10);
-    font-family: "Comfortaa";
-    font-size: 40px;
-    max-width: 100%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-    text-align: center;
-    border-radius: 20px;
-    margin-bottom: 50px;
-}
-
-h3 { 
-    color: rgb(0, 4, 10);
-    padding: 20px;
-    font-family: "Didact Gothic";
-    font-size: xx-large;
-    max-width: 100%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 300px;
-    text-align: center;
-    margin-bottom: 0px;
-    text-decoration-line: underline;
-}
-
-div.form-input-group {
-    color: rgb(0, 0, 0);
-    padding: 20px;
-    border-color: black;
-    font-family: "Didact Gothic";
-    font-size: small;
-    font-weight: bold;
-    max-width: 100%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 300px;
-    text-align: center;
-    /* margin-bottom: 30px; */
-}
-
-button {
-    color: rgb(250, 249, 249);
-    font-family: "Comfortaa";
-    font-size: large;
-    font-weight: bold;
-    background: rgb(6, 61, 94);
-    max-width: 100%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    padding-top: 20px;
-    padding-bottom: 18px;
-    padding-right: 1px;
-    padding-left: 1px;
-    width: 20%;
-    text-align: center;
-    border-radius: 25px;
-    margin-bottom: 80px;
-    overflow: hidden;
-    white-space: nowrap;
-}
-
-p {
-    font-family: "Didact Gothic";
-    font-weight: bold;
-    color: rgb(0, 0, 0);
-    max-width: 30%;
-    font-size: large;
-    display: block;
-    padding-top: 1px;
-    padding-bottom: 5px;
-    padding-right: 10px;
-    padding-left: 10px;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    /* background: rgb(142, 219, 238); */
-    border-radius: 10px;
-    overflow: hidden;
-    white-space: nowrap;
-}
-
-/* .alert {
-    text-align: center;
-    font-family: "Comfortaa";
+  margin-bottom: 20px;
 }
 
 label {
-  margin-right: 0.5rem;
-} */
-
-
-
-/* #modalContainer {
-	background-color:rgba(0, 0, 0, 0.3);
-	position:absolute;
-  top:0;
-	width:100%;
-	height:100%;
-	left:0px;
-	z-index:10000;
+  display: block;
+  font-size: 18px;
+  margin-bottom: 5px;
 }
 
-#alertBox {
-	position:relative;
-	width:33%;
-	min-height:100px;
-  max-height:400px;
-	margin-top:50px;
-	border:1px solid #fff;
-	background-color:#fff;
-	background-repeat:no-repeat;
-  top:30%;
+input[type="text"],
+input[type="password"] {
+  width: 95%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 }
 
-#modalContainer > #alertBox {
-	position:fixed;
+button[type="submit"] {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #fff;
 }
 
-#alertBox h1 {
-	margin:0;
-	font:bold 1em Raleway,arial;
-	background-color:#f97352;
-	color:#FFF;
-	border-bottom:1px solid #f97352;
-	padding:10px 0 10px 5px;
+button[type="submit"]:hover {
+  cursor: pointer;
+  background-color: #0069d9;
 }
 
-#alertBox p {
-	height:50px;
-	padding-left:5px;
-  padding-top:30px;
-  text-align:center;
-  vertical-align:middle;
+.alert {
+  padding: 10px;
+  margin-bottom: 20px;
+  color: #721c24;
+  background-color: #f8d7da;
+  border: 1px solid #f5c6cb;
+  border-radius: 5px;
 }
 
-#alertBox #closeBtn {
-	display:block;
-	position:relative;
-	margin:10px auto 10px auto;
-	padding:7px;
-	border:0 none;
-	width:70px;
-	text-transform:uppercase;
-	text-align:center;
-	color:#FFF;
-	background-color:#f97352;
-	border-radius: 0px;
-	text-decoration:none;
-  outline:0!important;
-} */
+.math {
+  font-size: 20px;
+  margin-top: 20px;
+  text-align: center;
+}
+
 
 </style>
