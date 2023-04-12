757de084788079228d2fe75ebe6297680979ccd5
Ang Young <angieyou3@gmail.com>
2023-04-11 20:17:02 -0400

    correct home.vue

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,13 +1,5 @@
-
 <template>
   <div class="home">
-<<<<<<< HEAD
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-
-   <class-list></class-list>
-    
-=======
     <h1 id="home">Home</h1>
     <p id="auth">Student View</p>
    <class-list></class-list>
@@ -16,29 +8,19 @@
   <math-fact class="mathfact"></math-fact>
   <p>{{ mathFact }}</p>
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa|Didact Gothic">
->>>>>>> dd457d2c99161adf0be951403142d88fd9081c9b
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa%7CDidact Gothic">
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import ClassList from '../components/ClassList.vue'
-
-=======
 import ClassList from '../components/ClassList.vue';
 import MathFact from "../components/MathFact.vue";
->>>>>>> dd457d2c99161adf0be951403142d88fd9081c9b
 
 
 export default {
   components: { 
       ClassList,
-<<<<<<< HEAD
-      
-=======
       MathFact
->>>>>>> dd457d2c99161adf0be951403142d88fd9081c9b
     },
 
   name: "home"
@@ -82,4 +64,3 @@ export default {
     font-family: "Comfortaa";
   }
 </style>
-
