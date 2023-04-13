dbaf731faacf6d4240049bd783099427f124b50e
Ang Young <angieyou3@gmail.com>
2023-04-13 18:09:22 -0400

    trying to get Accordion to work

--- vue/src/components/ProgrammingCurriculum.vue
+++ vue/src/components/ProgrammingCurriculum.vue
@@ -1,40 +1,40 @@
 <template>
   <div>
-    <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
     <h1 class="curriculum">Programming Curriculum:</h1>
-    <vue-accordion>
-      <vue-accordion-pane v-for="item in curriculum" :key="item.curriculumId">
-        <template #title>
-          <div class="pane-title">{{ item.curriculumName }}</div>
-        </template>
-        <template #content>
-          <div class="pane-content">
-            <p>{{ item.reading }}</p>
-          </div>
-        </template>
-      </vue-accordion-pane>
-    </vue-accordion>
+
+    <VueAccordion :multiple="true" :activeIndex="0">
+      <VueAccordionTab v-for="item in curriculum" :header="item.title" :key="item.id">
+        <p>{{ item.content }}</p>
+      </VueAccordionTab>
+    </VueAccordion>
   </div>
 </template>
 
 <script>
 import DatabaseService from "../services/DatabaseService.js";
-import { VueAccordion, VueAccordionPane } from "vue-accordion";
+import { VueAccordion, VueAccordionTab } from "vue-accordion";
+// import Accordion from 'primevue/accordion';
+// import AccordionTab from 'primevue/accordiontab';
 
 export default {
   name: "programmingcurriculum",
   components: {
     VueAccordion,
-    VueAccordionPane,
+    VueAccordionTab,
+
   },
+  
+
   data() {
     return {
       curriculum: [],
     };
   },
   created() {
+    console.log("Fetching curriculum data...");
     DatabaseService.getAllCurriculum().then((response) => {
       this.curriculum = response.data;
+      console.log("Curriculum data retrieved:", this.curriculum);
     });
   },
 };
@@ -47,22 +47,34 @@ export default {
   color: #f5a623;
   text-align: center;
 }
+.p-accordion {
+  width: 100%;
+}
 
+.p-accordion .p-accordion-tab {
+  border-bottom: 1px solid #ddd;
+}
 
+.p-accordion .p-accordion-header {
+  font-weight: bold;
+  background-color: #f5f5f5;
+  padding: 10px;
+  cursor: pointer;
+}
 
+.p-accordion .p-accordion-header:focus {
+  outline: none;
+}
 
-/* h1.classlist {
-    color: rgb(0, 0, 0);
-    padding: 20px;
-    font-family: "Didact Gothic";
-    font-size: 40px;
-    display: block;
-    margin-left: 70px;
-    margin-right: auto;
-    width: 100%;
-    margin-bottom: 0px;
-    text-decoration-line: underline;
-/* } */
+.p-accordion .p-accordion-header:hover {
+  background-color: #e5e5e5;
+}
+
+.p-accordion .p-accordion-content {
+  padding: 10px;
+  background-color: #fff;
+}
+
 
 
 
--- vue/src/views/Programming.vue
+++ vue/src/views/Programming.vue
@@ -1,75 +1,33 @@
 <template>
   <div>
-    <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-    <h1 class="curriculum">Programming Curriculum:</h1>
-    <vue-accordion>
-      <vue-accordion-pane v-for="item in curriculum" :key="item.curriculumId">
-        <template #title>
-          <div class="pane-title">{{ item.curriculumName }}</div>
-        </template>
-        <template #content>
-          <div class="pane-content">
-            <p>{{ item.reading }}</p>
-          </div>
-        </template>
-      </vue-accordion-pane>
-    </vue-accordion>
+
+    <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
+    
+   <programming-curriculum></programming-curriculum> 
+   <accordion></accordion>
+   <accordion-tab></accordion-tab>
+   
   </div>
 </template>
 
 <script>
-import DatabaseService from "../services/DatabaseService.js";
-import { VueAccordion, VueAccordionPane } from "vue-accordion";
+
+import ProgrammingCurriculum from '../components/ProgrammingCurriculum.vue';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 export default {
   name: "Programming",
   components: {
-    VueAccordion,
-    VueAccordionPane,
-  },
-  data() {
-    return {
-      curriculum: [],
-    };
-  },
-  created() {
-    console.log("Fetching curriculum data...");
-    DatabaseService.getAllCurriculum().then((response) => {
-      this.curriculum = response.data;
-      console.log("Curriculum data retrieved:", this.curriculum);
-    });
-  },
-};
+   ProgrammingCurriculum,
+     Accordion,
+    AccordionTab  
+  }
+}
 </script>
 
 
 
 <style>
-p#auth {
-  color: rgb(11, 12, 12);
-  font-family: "Comfortaa";
-  font-size: 30px;
-  max-width: 100%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-  text-align: center;
-  border-radius: 20px;
-  margin-bottom: 50px;
-}
 
-h1#home {
-  color: rgb(0, 0, 0);
-  font-family: "Comfortaa";
-  font-size: 40px;
-  max-width: 100%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-  text-align: center;
-  border-radius: 20px;
-  margin-bottom: 50px;
-}
 </style>
