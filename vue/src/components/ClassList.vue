01ea30df51ebe5b76c86e6259df33cc3b157c5ae
Rose Dickson <rosemargaret106@gmail.com>
2023-04-13 09:10:49 -0400

    classlist prevent merge conflict

--- vue/src/components/ClassList.vue
+++ vue/src/components/ClassList.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="header">
-    <img src="../img/image.png" alt="super-smart" />
-
+  <div class="header-image">
+  <img src="../img/superSmartBanner.jpg" alt="super-smart" />
+</div>
+<router-link
+  v-bind:to="{ name: 'logout' }"
+  v-if="$store.state.token != ''"
+  class="table-link"
+>
+  Logout
+</router-link>
     <h1 class="classlist">Class List:</h1>
     <table>
-      <!-- <thead>
-              <tr>
-                  <th class="math">Math</th>
-                  <th class="science">Science</th>
-                  <th class="cp">Computer Programming</th>
-              </tr>
-          </thead> -->
       <tbody>
+        <tr>
+          <th>Course Name</th>
+          <th>Course Description</th>
+          <th>Cost</th>
+        </tr>
         <tr v-for="item in classes" v-bind:key="item.courseId">
           <td>{{ item.courseName }}</td>
           <td>{{ item.courseDescription }}</td>
+          <td> $ {{ item.cost }} </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-
 <script>
 import DatabaseService from "../services/DatabaseService.js";
-
 export default {
   name: "class-list",
   data() {
@@ -38,66 +43,73 @@ export default {
   },
 };
 </script>
-
 <style>
 .header {
   position: relative;
   text-align: center;
 }
-
 .header img {
   width: 100%;
   max-height: 50%;
 }
-
 .classlist {
   font-size: 36px;
-  color: #5f9ea0;
+  color: #5F9EA0;
   text-align: center;
 }
-
 table {
-  border-collapse: collapse;
+  border-collapse: separate;
+  border-spacing: 0;
   margin: 20px auto;
   width: 80%;
 }
-
 th,
 td {
-  border: 1px solid #ccc;
+  border: none;
   padding: 12px;
   text-align: left;
 }
-
 th {
-  background-color: #f5a623;
+  background-color: #82B3A4;
   color: white;
   font-weight: bold;
   text-transform: uppercase;
 }
-
 tr:nth-child(even) {
-  background-color: #f8f8f8;
+  background-color: #F8F8F8;
 }
-
 tr:hover {
-  background-color: #f5f5f5;
+  background-color: #F5F5F5;
   transition: background-color 0.3s ease;
 }
-
 td:first-child {
-  color: #f5a623;
+  color: #F5A623;
   font-weight: bold;
 }
-
 td:nth-child(2) {
-  color: #7f7f7f;
+  color: #7F7F7F;
 }
-
 tbody {
   font-family: "Comfortaa", sans-serif;
 }
-
+.table-link {
+  color: #5F9EA0;
+  font-family: "Comfortaa", sans-serif;
+  font-size: 20px;
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: 8px 8px;
+  border-radius: 10px;
+  background-color: #F8F8F8;
+  margin: 0 5px;
+  transition: background-color 0.3s ease;
+  float: right;
+}
+.table-link:hover {
+  background-color: #5F9EA0;
+  color: #F8F8F8;
+  transition: background-color 0.3s ease;
+}
 /* h1.classlist {
     color: rgb(0, 0, 0);
     padding: 20px;
@@ -110,7 +122,6 @@ tbody {
     margin-bottom: 0px;
     text-decoration-line: underline;
 }
-
 table {
     color: rgb(0, 0, 0);
     padding: 20px;
@@ -122,7 +133,6 @@ table {
     width: 100%;
     margin-bottom: 0px;
 }
-
 th {
     border: thick dotted rgb(0, 0, 0);
     margin: auto;
