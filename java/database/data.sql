71b53dfe8770ae4a977edb21df25b386fcfcef09
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-04-16 14:38:29 -0400

    data.sql save

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,11 +2,12 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO users (username,password_hash,role) VALUES ('meghan','$2a$10$MXWhj.tMfEHlnKs6GhXFfeDs3Y6NNoTGXt9Xn4CdLs59aZR28qdDa','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('Meghan','$2a$10$MXWhj.tMfEHlnKs6GhXFfeDs3Y6NNoTGXt9Xn4CdLs59aZR28qdDa','ROLE_ADMIN');
 INSERT INTO users (username,password_hash,role) VALUES ('student1','asdfad','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('student2','adfadf','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('student4','asdfadfg','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('student5','qewrqwe','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('student3','asdfadfg','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('student4','qewrqwe','ROLE_USER');
+
 
 INSERT INTO course (course_id, course_name, course_description, difficulty) VALUES ('M1', 'Math','Basic Math','Beginner');
 INSERT INTO course (course_id, course_name, course_description, difficulty) VALUES ('S2', 'Science','This is a science class','Intermediate');
@@ -22,7 +23,7 @@ The chemical formula for glucose is C6H12O6.',
 			'links'
 		   );
 INSERT INTO curriculum (curriculum_id, course_id, curriculum_name, reading, homework, resources) 
-	VALUES  ('S2C2',	
+	VALUES  ('S2C2',
 			'S2',
 			'Chapter 2: Physics of Motion',
 			'The acceleration due to gravity on Earth is approximately 9.8 meters per second squared (32 feet per second squared). The speed of light in a vacuum is approximately 299,792,458 meters per second (or about 186,000 miles per second).
@@ -39,67 +40,61 @@ INSERT INTO curriculum (curriculum_id, course_id, curriculum_name, reading, home
 			'links'
 		   );				   
 
-INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
-	VALUES (
-		'S2C1',
-		(SELECT user_id FROM users where username = 'user'),
-		'2023-04-11',
-		'FALSE'
-	);
 
-INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
-	VALUES (
-		'S2C2',
-		(SELECT user_id FROM users where username = 'user'),
-		'2023-04-11',
-		'FALSE'
-	);
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'user'),'S2');
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'student1'),'S2');
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'student1'),'M1');
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'student2'),'S2');
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'student3'),'M1');			
+INSERT INTO student_class(student_id, course_id)
+	VALUES ((SELECT user_id FROM users where username = 'student4'),'M1');
 
 
+
 INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
-	VALUES (
-		'S2C3',
-		(SELECT user_id FROM users where username = 'user'),
-		'2023-04-11',
-		'FALSE'
-	);
+	VALUES ('S2C1',(SELECT user_id FROM users where username = 'user'),'2023-04-11','FALSE');
+INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
+	VALUES ('S2C2',(SELECT user_id FROM users where username = 'student1'),'2023-04-11','FALSE');
+INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
+	VALUES ('S2C3',(SELECT user_id FROM users where username = 'student2'),'2023-04-11','FALSE');
+INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
+	VALUES ('S2C3',(SELECT user_id FROM users where username = 'student3'),'2023-04-11','FALSE');
+INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
+	VALUES ('S2C1',(SELECT user_id FROM users where username = 'student3'),'2023-04-11','FALSE');
 
 	
-INSERT INTO grade (student_id, assignment_id, grade) 
-VALUES ((SELECT user_id FROM users where username = 'user'),
-		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C1'),
-		'A');
+-- INSERT INTO grade (student_id, assignment_id, grade) 
+-- VALUES ((SELECT user_id FROM users where username = 'user'),
+-- 		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C1'),
+-- 		'A');
 		
 			
-INSERT INTO grade (student_id, assignment_id, grade) 
-VALUES ((SELECT user_id FROM users where username = 'user'),
-		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C2'),
-		'A');
+-- INSERT INTO grade (student_id, assignment_id, grade) 
+-- VALUES ((SELECT user_id FROM users where username = 'user'),
+-- 		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C2'),
+-- 		'A');
 			
-INSERT INTO grade (student_id, assignment_id, grade) 
-VALUES ((SELECT user_id FROM users where username = 'user'),
-		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C3'),
-		'A');
+-- INSERT INTO grade (student_id, assignment_id, grade) 
+-- VALUES ((SELECT user_id FROM users where username = 'user'),
+-- 		(SELECT assignment_id from assignment WHERE curriculum_id = 'S2C3'),
+-- 		'A');
 
 
-INSERT INTO student_class(student_id, course_id)
-	VALUES ((SELECT user_id FROM users where username = 'user'),
-			'S2');
-
-INSERT INTO student_class(student_id, course_id)
-	VALUES ((SELECT user_id FROM users where username = 'student1'),
-			'S2');
-			
-INSERT INTO student_class(student_id, course_id)
-	VALUES ((SELECT user_id FROM users where username = 'student2'),
-			'S2');
-
-INSERT INTO student_class(student_id, course_id)
-	VALUES ((SELECT user_id FROM users where username = 'student1'),
-			'M1');
-			
-INSERT INTO student_class(student_id, course_id)
-	VALUES ((SELECT user_id FROM users where username = 'student3'),
-			'M1');
+--  INSERT INTO student_class(student_id, course_id)
+-- 	VALUES ((SELECT user_id FROM users where username = 'kid'),'M1');
+--  INSERT INTO student_class(student_id, course_id)
+-- 	VALUES ((SELECT user_id FROM users where username = 'kid'),'S2');	
+-- INSERT INTO assignment (curriculum_id, student_id, submission_date,status) 
+-- VALUES ('S2C3',(SELECT user_id FROM users where username = 'kid'),'2023-04-11','FALSE');
+	
+	
 
 COMMIT TRANSACTION;
+
+
+
